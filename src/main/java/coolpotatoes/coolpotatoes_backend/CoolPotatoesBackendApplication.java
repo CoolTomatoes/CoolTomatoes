67f1a6b3c98b232d6030579416f6ec5ea8db2f1b
daye200 <daye200@naver.com>
2024-12-23 22:17:53 +0900

    첫번째 커밋

---
+++ src/main/java/coolpotatoes/coolpotatoes_backend/CoolPotatoesBackendApplication.java
@@ -0,0 +1,13 @@
+package coolpotatoes.coolpotatoes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolPotatoesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolPotatoesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/coolpotatoes/coolpotatoes_backend/CoolPotatoesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package coolpotatoes.coolpotatoes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolPotatoesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
