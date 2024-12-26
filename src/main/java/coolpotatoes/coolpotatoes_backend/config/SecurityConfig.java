1e190d63ac1215c12f3870daffcbb9c6937d5a1e
daye200 <daye200@naver.com>
2024-12-26 19:00:49 +0900

    feat(이런저런):기능들

---
+++ src/main/java/coolpotatoes/coolpotatoes_backend/config/SecurityConfig.java
@@ -0,0 +1,132 @@
+package coolpotatoes.coolpotatoes_backend.config;
+//
+//import coolpotatoes.coolpotatoes_backend.filter.JWTFilter;
+//import coolpotatoes.coolpotatoes_backend.filter.LoginFilter;
+import coolpotatoes.coolpotatoes_backend.filter.JWTFilter;
+import coolpotatoes.coolpotatoes_backend.jwt.JWTUtil;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+///*새날지기코드*/
+////@Configuration
+////@EnableWebSecurity
+////@RequiredArgsConstructor
+////public class SecurityConfig{
+////
+////    /*
+////      Authentication 은 프론트에서 외부 API를 통해 진행
+////      서버측에서는 프론트에서 사용자 id, pw, 이름, 학과, 학번, 학생상태 가 넘어오면 JWT access token 발급
+////
+////      이후 프론트에서 요청이 올 경우 헤더의 JWT를 JWT Filter를 통해 사용자 정보를 확인
+////     */
+////
+////    private final JWTUtil jwtUtil;
+////
+////    @Bean
+////    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+////
+////        http
+////                .csrf(auth -> auth.disable());
+////
+////        http
+////                .formLogin(auth -> auth.disable());
+////
+////        http
+////                .httpBasic(auth -> auth.disable());
+////
+////        http
+////                .cors(auth -> auth.disable());
+////
+////
+////        http
+////                .authorizeHttpRequests(auth -> auth
+////                        .requestMatchers("/", "/login", "/docs/**",
+////                                "/v3/api-docs/**", "/swagger-ui/**", "/ws-stomp/**").permitAll()
+////                        .anyRequest().authenticated());
+////
+////
+////        // 필터 등록
+////        http
+////                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+////
+////
+////        http
+////                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+////
+////        return http.build();
+////    }
+////
+////
+////}
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil) {
+
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //csrf disable
+        http
+                .csrf((auth) -> auth.disable());
+
+
+        http
+                .cors(auth -> auth.disable());
+
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+
+                        .requestMatchers("/", "/login", "/docs/**", "/swagger-ui.html",
+                                "/v3/api-docs/**", "/swagger-ui/**", "/ws-stomp/**").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+
+
+        //세션 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
+
---
+++ src/main/java/coolpotatoes/coolpotatoes_backend/jwt/JWTUtil.java
@@ -0,0 +1,88 @@
+package coolpotatoes.coolpotatoes_backend.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+/*새날지기 코드 가져온거*/
+//@Component
+//public class JWTUtil {
+//
+//    private final SecretKey secretKey;
+//
+//    /* access token 유효시간 */
+//    private final long accessTokenValidity = 1000 * 60 * 60; // 60분
+//
+//
+//    /* SecretKey 설정 */
+//    public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
+//        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+//    }
+//
+//    /* token에서 username 꺼내기 */
+//    public String getUsername(String token) {
+//        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+//    }
+//
+//    /* token 만료시간 확인 */
+//    public Boolean isExpired(String token) {
+//        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+//    }
+//
+//    /* 유효한 token인지 확인 */
+//    public boolean validateToken(String token, String username) {
+//        return (username.equals(getUsername(token)) && !isExpired(token));
+//    }
+//
+//    /* AccessToken 발급 */
+//    public String createAccessToken(String username) {
+//        return Jwts.builder()
+//                .claim("username", username)
+//                .issuedAt(new Date(System.currentTimeMillis()))
+//                .expiration(new Date(System.currentTimeMillis() + accessTokenValidity))
+//                .signWith(secretKey)
+//                .compact();
+//    }
+//}
+
+@Component
+public class JWTUtil {
+
+    private SecretKey secretKey;
+
+    public JWTUtil(@Value("${spring.jwt.secret}")String secret) {
+
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String getUsername(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    public String getRole(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+
+    public String createJwt(String username, String role, Long expiredMs) {
+
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
